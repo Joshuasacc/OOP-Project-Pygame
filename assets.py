aca1710ddfdc92ee4803bb52277b4062a2c31eb9
Alemar-20 <alimar13salic@gmail.com>
2025-10-19 22:23:49 +0800

    Update assets.py

--- assets.py
+++ assets.py
@@ -3,13 +3,24 @@ import gamesetting as gs
 
 class Assets:
     def __init__(self):
-        self.sprite_sheet = self.load_sprite_sheet("images", "BombermanLandPSP-Bomberman.png", 192*4, 272*4)
-        self.player_char = self.load_sprite_range(gs.PLAYER, self.sprite_sheet)
+        # DO NOT SCALE the sprite sheet if it's already the correct size (325x257)
+        # Assuming "owncreation.png" is the 325x257 image
+        self.sprite_sheet = self.load_sprite_sheet("images", "owncreation.png") # Removed hardcoded size
 
-    def load_sprite_sheet(self, path, file_name, width, height):
-        """Load a sprite sheet and split it into individual sprites.""" 
+        self.player_char = self.load_sprite_range(
+            gs.PLAYER, 
+            self.sprite_sheet, 
+            row=gs.SPRITE_HEIGHT, # Use new constants
+            col=gs.SPRITE_WIDTH, 
+            width=gs.SPRITE_WIDTH, 
+            height=gs.SPRITE_HEIGHT,
+            resize=True # Added resize to scale it up to a visible game size, e.g., 32x32
+        )
+
+    def load_sprite_sheet(self, path, file_name): # Removed width, height arguments
+        """Load a sprite sheet.""" 
         image = pygame.image.load(f"{path}/{file_name}").convert_alpha()
-        image = pygame.transform.scale(image, (width, height))
+        # image = pygame.transform.scale(image, (width, height)) # REMOVED SCALING
         return image
     
     def load_sprites(self, spritesheet, xcoord, ycoord, width, height):
@@ -24,14 +35,24 @@ class Assets:
         image.set_colorkey(gs.BLACK)
         return image
     
-    def load_sprite_range(self, image_dict, spritesheet, row=gs.SIZE, col=gs.SIZE, width=gs.SIZE, height=gs.SIZE, resize=False):
+    def load_sprite_range(self, image_dict, spritesheet, row, col, width, height, resize=False): # Changed defaults
         """Return a dictionary containing list of images for the animation."""
         animation_images = {}
         for animation in image_dict.keys():
             animation_images[animation] = []
             for coord in image_dict[animation]:
-                image = self.load_sprites(spritesheet, coord[1] * col, coord[0] * row, width, height)
+                # NOTE: The coordinates here are (ROW, COL) * (SPRITE_HEIGHT, SPRITE_WIDTH)
+                # You were using coord[1] * col (COL * WIDTH) for x and coord[0] * row (ROW * HEIGHT) for y
+                # This is correct for (ROW, COL) mapping if row/col are pixel sizes
+                image = self.load_sprites(
+                    spritesheet, 
+                    coord[1] * col, # X-coordinate: Column index * Sprite Width
+                    coord[0] * row, # Y-coordinate: Row index * Sprite Height
+                    width, 
+                    height
+                )
                 if resize:
-                    image = pygame.transform.scale(image, (32, 32))
+                    # Scale to a more suitable game size, like 32x64 or 64x64
+                    image = pygame.transform.scale(image, (32, 64)) # Example scale
                 animation_images[animation].append(image)
         return animation_images
